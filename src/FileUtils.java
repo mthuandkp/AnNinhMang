602298e1f5c3f26d4540f5aa073b3c70691ef7b2
mthuandkp <pnmthuan@gmail.com>
2023-02-27 16:40:14 +0700

    add source file

---
+++ src/FileUtils.java
@@ -0,0 +1,16 @@
+package src;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class FileUtils {
+    private final List<String> COMMON_URL = Arrays.asList(
+            //Windows
+            "C:\\Users\\",
+            //Linux,Mac
+            "/Users"
+    );
+    public static List<String> readAllFileinFolder(String folderURL){
+        return null;
+    }
+}
